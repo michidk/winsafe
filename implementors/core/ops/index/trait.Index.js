beafdff3a6506035b2ecd3169573fa2ba00da347
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-01-22 14:35:12 -0300

    Cargo doc deployment 2021-01-22 14:35:12.

--- implementors/core/ops/index/trait.Index.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["winsafe"] = [{"text":"impl Index&lt;usize&gt; for RadioGroup","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["winsafe"] = [{"text":"impl Error for ERROR","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
