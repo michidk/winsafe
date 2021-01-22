f655db5b80cd9e02f41e626aee9f6a88b5fffa7e
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-01-22 14:28:21 -0300

    Cargo doc deployment 2021-01-22 14:28:20.

--- doc/implementors/core/convert/trait.AsRef.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["winsafe"] = [{"text":"impl AsRef&lt;GUID&gt; for CLSID","synthetic":false,"types":[]},{"text":"impl AsRef&lt;GUID&gt; for IID","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
