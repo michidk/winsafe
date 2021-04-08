9a41164de4b36d88fb4e6fbd4dc3ea0a8aef9d9f
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-04-08 08:38:54 -0300

    Cargo doc deployment 2021-04-08 08:38:51.

---
+++ winsafe/dshow/clsid/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["EnhancedVideoRenderer",""],["FilterGraph",""],["MR_BUFFER_SERVICE",""],["MR_VIDEO_ACCELERATION_SERVICE",""],["MR_VIDEO_MIXER_SERVICE",""],["MR_VIDEO_RENDER_SERVICE",""],["VIDEO_ZOOM_RECT",""]]});
