a9ef0181013b439d8046a8acc9bab8e38fba23ec
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-08-16 10:32:21 -0300

    Cargo doc deployment 2021-08-16 10:32:19.

--- winsafe/msg/tbm/sidebar-items.js
+++ winsafe/msg/tbm/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["AddBitmap","`TB_ADDBITMAP` message parameters."],["AddButtons","`TB_ADDBUTTONS` message parameters."]]});
+initSidebarItems({"struct":[["AddBitmap","`TB_ADDBITMAP` message parameters."],["AddButtons","`TB_ADDBUTTONS` message parameters."],["AddString","`TB_ADDSTRING` message parameters."],["AutoSize","`TB_AUTOSIZE` message, which has no parameters."],["ButtonCount","`TB_BUTTONCOUNT` message, which has no parameters."]]});
