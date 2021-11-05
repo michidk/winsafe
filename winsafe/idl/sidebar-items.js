e9d0d338aea2ef700c76b1b279996a47c25d069e
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-11-05 10:48:41 -0300

    Cargo doc auto deployment 2021-11-05 10:48:39.

--- winsafe/idl/sidebar-items.js
+++ winsafe/idl/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["vt","IDL COM virtual tables."]],"struct":[["IPersist","`IPersist` COM interface over `IPersistVT`."]]});
+initSidebarItems({"mod":[["vt","IDL COM virtual tables."]],"struct":[["IPersist","`IPersist` COM interface over `IPersistVT`."]],"trait":[["IPersistT","Exposes the `IPersist` methods."]]});
